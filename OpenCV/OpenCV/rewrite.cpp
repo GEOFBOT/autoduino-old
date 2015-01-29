072164c36009f4dc2ef048ec24a678bb9b7114c1
Geoffrey Mon <geofbot@gmail.com>
2015-01-28 23:05:29 -0500

    Rewrite: use the bottom half of the image

--- OpenCV/OpenCV/rewrite.cpp
+++ OpenCV/OpenCV/rewrite.cpp
@@ -211,6 +211,7 @@ int main()
 		}
 
 		// Process image
+		frame = frame(Rect(0, cvRound(0.5*frame.rows), frame.cols, cvRound(0.5*frame.rows)));
 		frame.copyTo(display);
 		bilateralFilter(frame, smoothed, 9, 75, 75);
 		cvtColor(smoothed, smoothed, COLOR_BGR2GRAY);
@@ -227,7 +228,7 @@ int main()
 		//addWeighted(processed, 0.1, combined, 0.9, 0.0, combined);
 		Canny(smoothed, canny, threshold1, threshold2);
 		HoughLines(canny, lines, 1, radian(1), 90);
-		HoughLines(processed, lines_threshold, 1, radian(1), 90);
+		HoughLines(processed, lines_threshold, 1, radian(1), 60);
 		cvtColor(thresh, thresh, COLOR_GRAY2BGR);
 		cvtColor(processed, processed, COLOR_GRAY2BGR);
 
@@ -246,11 +247,11 @@ int main()
 			
 			for (int j = 0; j < it.count; ++j, ++it) {
 				if (nearRange(it.pos().y, 0, thresh.rows)
-					&& nearRange(it.pos().x - 10, 0, thresh.cols)
-					&& nearRange(it.pos().x + 10, 0, thresh.cols)) {
+					&& nearRange(it.pos().x - 20, 0, thresh.cols)
+					&& nearRange(it.pos().x + 20, 0, thresh.cols)) {
 					++total;
-					Point a = Point(it.pos().x - 10, it.pos().y);
-					Point b = Point(it.pos().x + 10, it.pos().y);
+					Point a = Point(it.pos().x - 20, it.pos().y);
+					Point b = Point(it.pos().x + 20, it.pos().y);
 					if (thresh.at<Vec3b>(a) != Vec3b(0,0,0) && thresh.at<Vec3b>(b) != Vec3b(0,0,0)) {
 						circle(thresh, it.pos(), 2, Scalar(0, 0, 255));
 						++good;
